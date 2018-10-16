e90f1ef912d39a3c54f67bde5e5fe626e9027032
amckean12 <amckean@me.com>
2018-10-16 13:38:31 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,35 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+undefined
+element
+<div>​</div>​
+element.innerHTML  = 'Hello, Alex'
+"Hello, Alex"
+element
+<div>​Hello, Alex​</div>​
+element.style.backgroundColor = '#f9f9f9'
+"#f9f9f9"
+element
+<div style=​"background-color:​ rgb(249, 249, 249)​;​">​Hello, Alex​</div>​
+document.body.appendChild(element)
+<div style=​"background-color:​ rgb(249, 249, 249)​;​">​Hello, Alex​</div>​
+element.style.textAlign = 'center'
+"center"
+var ul =- document.createElement('ul')
+undefined
+var ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul);
+<ul>​…​</ul>​
+ul.style.textAlign = 'left'
+"left"
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+<li>​2​</li>​
+ul.remove()
